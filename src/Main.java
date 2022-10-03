177d45818f9c6c4b29dfb3b272173cc63df85596
леново <tolubaikyzydilnaz@gmail.com>
2022-10-03 11:54:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//презентация тапшырма №1.1
+//    method(10,20,11);
+//}
+//    static void  method(int a,int b,int c) {
+//        if (a > b && a > c && b > c) {
+//            System.out.printf("%d %d %d", a, b, c);
+//        } else if (a > b && a > c && c > b) {
+//            System.out.printf("%d %d %d", a, c, b);
+//        } else if (b > a && b > c && a > c) {
+//            System.out.printf("%d %d %d", b, a, c);
+//        } else if (b > a && b > c && c > a) {
+//            System.out.printf("%d %d %d", b, c, a);
+//        } else if (c > a && c > b && a > b) {
+//            System.out.printf("%d %d %d", c, a, b);
+//        } else {
+//            System.out.printf("%d %d %d", c, b, a);
+//№1.2
+//       }
+//        static void met(int a,int b,int c){//9 11 8
+//            int x;
+//            if (a>b){//false
+//                x=a;//
+//                a=b;//
+//                b=x;//
+//            }if (b>c){//11>8
+//                x=b;//11
+//                b=c;//8
+//                c=x;//c=11  a9 b8 c11
+//            }if (a>b){//true
+//                x=a;//x=9
+//                a=b;//a=8
+//                b=x;//b=9
+//            }//a:8 b=9 c=11
+//            System.out.println(a+" "+b+" " +
+//                    " "+c);
+//        }
+//
+//        System.out.println(method(9));
+//    }
+//    static String method(int san) {
+//        if (san % 2 == 0) {
+//            return "жуп  сан";
+//
+//        } else {
+//            return "так сан";
+//
+//        }
+//        System.out.println(method(12,34,56));
+//
+//    }
+//    static double method(int san,int san1,int san2){
+//        return  (san+san1+san2)/3d;
+//
+//        System.out.println(method(30));
+//    }
+//
+//    static int method(int san) {
+//        int sum = 0;
+//        for (int i = 0; i <= san; i++) {
+//            sum = sum+i;
+//        }
+//        return sum;
+//        System.out.println(method(new int[]{1,2,3,4,5}));
+//
+//    }
+//    static int method (int [] san ){
+//        int minSan = san[0];
+//        for (int i = 0; i < san.length; i++) {
+//         if (san[i]<san[0]){
+//             minSan=san[i];
+//         }
+//        }
+//        return (minSan);
+
+    }
+}
